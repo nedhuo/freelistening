f59e0d024bc2f39b5e46098ecbe1a492240437c4
Admin <nedhuo@163.com>
2022-05-31 21:34:42 +0800

    1

--- lib_common/src/main/java/com/nedhuo/lib_common/constants/RouteConstants.kt
+++ lib_common/src/main/java/com/nedhuo/lib_common/constants/RouteConstants.kt
@@ -1,3 +1,5 @@
 package com.nedhuo.lib_common.constants
 
 const val SIX_MONTH_FRAGMENT_HOME = "/module_listening/six_month_fragment_home"
+
+const val SIX_MONTH_ACTIVITY_BOOK_DETAIL = "/module_listening/six_month_activity_book_detail"
--- module_listening/src/main/java/com/nedhuo/module_listening/fragment/SixMonthHomeFragment.kt
+++ module_listening/src/main/java/com/nedhuo/module_listening/fragment/SixMonthHomeFragment.kt
@@ -1,9 +1,20 @@
 package com.nedhuo.module_listening.fragment
 
+import android.view.View
+import android.widget.AdapterView
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.alibaba.android.arouter.facade.annotation.Route
+import com.alibaba.android.arouter.launcher.ARouter
+import com.blankj.utilcode.util.StringUtils
+import com.blankj.utilcode.util.ToastUtils
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.listener.OnItemClickListener
+import com.chad.library.adapter.base.viewholder.BaseViewHolder
+import com.nedhuo.lib_base.BaseViewModel
 import com.nedhuo.lib_base.utils.BaseFragment
+import com.nedhuo.lib_common.bean.db.model.SixMonthBookModel
+import com.nedhuo.lib_common.constants.SIX_MONTH_ACTIVITY_BOOK_DETAIL
 import com.nedhuo.lib_common.constants.SIX_MONTH_FRAGMENT_HOME
 import com.nedhuo.module_listening.adapter.SixMonthBookAdapter
 import com.nedhuo.module_listening.databinding.ListeningFragmentSixMonthHomeBinding
@@ -39,6 +50,19 @@ class SixMonthHomeFragment :
         mViewModel.dataUpdate.observe(this) {
             mBookAdapter?.setNewInstance(it)
         }
+
+        mBookAdapter?.setOnItemClickListener(object : OnItemClickListener {
+
+            override fun onItemClick(adapter: BaseQuickAdapter<*, *>, view: View, position: Int) {
+                val sixMonthBookModel = adapter.getItem(position) as SixMonthBookModel
+                if (StringUtils.isEmpty(sixMonthBookModel.bookSuffix)) {
+                    ToastUtils.showShort("当前文件暂无法播放")
+                    return
+                }
+                ARouter.getInstance().build(SIX_MONTH_ACTIVITY_BOOK_DETAIL)
+                    .withString("bookSuffix", sixMonthBookModel.bookSuffix).navigation()
+            }
+        })
 
     }
 
